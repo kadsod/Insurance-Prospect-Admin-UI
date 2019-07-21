5c9d179d4788216449a8446d7855f35038e17acb
sNova29 <ksa.supernova@gmail.com>
2019-07-21 23:07:07 +0530

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TableComponent } from './table/table.component';
+import { NgProgressModule } from '@ngx-progressbar/core';
+import { NgProgressHttpModule } from '@ngx-progressbar/http';
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UiModule } from './ui/ui.module';
+import { AgGridModule } from 'ag-grid-angular';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    ToastrModule.forRoot(),
+    BrowserAnimationsModule,
+    AgGridModule.withComponents([]),
+    NgProgressModule.withConfig({
+      trickleSpeed: 200,
+      min: 20,
+      meteor: true,
+      color: 'orange'
+    }),
+    NgProgressHttpModule,
+    UiModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, TableComponent]
 })
 export class AppModule { }
---
+++ src/app/table/table.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-table',
+  templateUrl: './table.component.html'
+})
+export class TableComponent implements OnInit {
+  columnDefs = [
+    {headerName: 'Lead Score', field: 'score', sortable: true, filter: true},
+    {headerName: 'First Name', field: 'firstName', sortable: true, filter: true},
+    {headerName: 'Last Name', field: 'lastName', sortable: true, filter: true},
+    {headerName: 'Mobile Number', field: 'mobileNumber', sortable: true, filter: true},
+    {headerName: 'Email', field: 'emailId', sortable: true, filter: true},
+    {headerName: 'Area', field: 'location', sortable: true, filter: true},
+    {headerName: 'Zip Code', field: 'zip', sortable: true, filter: true},
+    {headerName: 'Gender', field: 'gender', sortable: true, filter: true},
+    {headerName: 'Message', field: 'enquiryMessage', sortable: true, filter: true},
+];
+
+rowData: any;
+
+constructor(private http: HttpClient) {
+
+}
+
+ngOnInit() {
+    this.rowData = this.http.get('https://insurance-prospect-backend.cfapps.io/data/getAllUsers');
+}
+
+}
