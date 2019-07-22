a4856ba9e5fa360d0cb26465894df837d8c35231
sNova29 <ksa.supernova@gmail.com>
2019-07-22 22:24:36 +0530

    Export to spreadsheet changes

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { GridOptions } from 'ag-grid-community';
 
 @Component({
   selector: 'app-table',
@@ -7,25 +8,36 @@ import { HttpClient } from '@angular/common/http';
 })
 export class TableComponent implements OnInit {
   columnDefs = [
-    {headerName: 'Lead Score', field: 'score', sortable: true, filter: true},
-    {headerName: 'First Name', field: 'firstName', sortable: true, filter: true},
-    {headerName: 'Last Name', field: 'lastName', sortable: true, filter: true},
-    {headerName: 'Mobile Number', field: 'mobileNumber', sortable: true, filter: true},
-    {headerName: 'Email', field: 'emailId', sortable: true, filter: true},
-    {headerName: 'Area', field: 'location', sortable: true, filter: true},
-    {headerName: 'Zip Code', field: 'zip', sortable: true, filter: true},
-    {headerName: 'Gender', field: 'gender', sortable: true, filter: true},
-    {headerName: 'Message', field: 'enquiryMessage', sortable: true, filter: true},
-];
+    { headerName: 'Lead Score', field: 'score', sortable: true, filter: true },
+    { headerName: 'First Name', field: 'firstName', sortable: true, filter: true },
+    { headerName: 'Last Name', field: 'lastName', sortable: true, filter: true },
+    { headerName: 'Mobile Number', field: 'mobileNumber', sortable: true, filter: true },
+    { headerName: 'Email', field: 'emailId', sortable: true, filter: true },
+    { headerName: 'Area', field: 'location', sortable: true, filter: true },
+    { headerName: 'Zip Code', field: 'zip', sortable: true, filter: true },
+    { headerName: 'Gender', field: 'gender', sortable: true, filter: true },
+    { headerName: 'Message', field: 'enquiryMessage', sortable: true, filter: true },
+  ];
 
-rowData: any;
+  rowData: any;
+  gridOptions: GridOptions;
+  public gridApi: any;
 
-constructor(private http: HttpClient) {
+  onBtExport(params: any) {
+    this.gridApi.exportDataAsCsv(params);
+  };
 
-}
+  onGridReady(params: any) {
+    this.gridApi = params.api;
+  }
 
-ngOnInit() {
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit() {
     this.rowData = this.http.get('https://insurance-prospect-backend.cfapps.io/data/getAllUsers');
-}
+  }
 
 }
